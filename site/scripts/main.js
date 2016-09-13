214798cd6125c6f5c3f7abdd9a7c0e41c6c2dca1
talrez <tal@way2cu.com>
2016-09-13 10:06:33 +0300

    Added section portfolio

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -52,19 +52,90 @@ Site.on_load = function() {
 	if (Site.is_mobile())
 		Site.mobile_menu = new Caracal.MobileMenu();
 
+	// create lightbox object for client logos images
+	Site.logo_lightbox = new LightBox('a.image', false, false, true);
+
+	// create lightbox object for portfolio gallery images
+	Site.portfolio_lightbox = new LightBox('a.portfolio', false, false, true);
+
 	// create slider for clients gallery
-		Site.client_gallery = new Caracal.Gallery.Slider();
-		Site.client_gallery
-			.controls.attach_next($('div.slider a.next'))
-			.controls.attach_previous($('div.slider a.previous'))
-			.controls.set_auto(3000)
-			.images.set_container(' div.slider')
-			.images.add('div.slider a.image')
-			.images.set_visible_count(5)
-			.images.set_center(true)
-			.images.set_spacing(20);
-		
-		Site.client_gallery.images.update();
+	Site.client_gallery = new Caracal.Gallery.Slider();
+	Site.client_gallery
+		.controls.attach_next($('div.slider a.next'))
+		.controls.attach_previous($('div.slider a.previous'))
+		.controls.set_auto(3000)
+		.images.set_container(' div.slider')
+		.images.add('div.slider a.image')
+		.images.set_visible_count(5)
+		.images.set_center(true)
+		.images.set_spacing(20);
+	Site.client_gallery.images.update();
+
+	// create slider for portfolio gallery
+	Site.portfolio_gallery = new Caracal.Gallery.Slider();
+	Site.portfolio_gallery
+		.controls.attach_next($('div.controls a.next'))
+		.controls.attach_previous($('div.controls a.previous'))
+		.images.set_container(' div.gallery')
+		.images.add('div.gallery a.portfolio')
+		.images.set_visible_count(3)
+		.images.set_center(true)
+		.images.set_spacing(20);
+	Site.portfolio_gallery.images.update();
+
+	/**
+	 * Constructor function for gallery images 
+	 */
+	function make_image(data) {
+		var link = $('<a>');
+		link
+			.attr('href', data.image)
+			.addClass("portfolio")
+
+		var thumbnail = $('<img>').appendTo(link);
+		thumbnail
+			.attr('src', data.thumbnail)
+			.attr('alt', data.title)
+
+		var information = $('<div class="information">').appendTo(link);
+		var title = $('<h4>').appendTo(information);
+		title
+			.text(data.title);
+		var para = $(data.description).appendTo(information);
+
+		return link;
+	}
+
+	/**
+	 * Callback function after images are loaded
+	 */
+	function image_loaded() {
+		Site.portfolio_lightbox = new LightBox('a.portfolio', false, false, true);
+	}
+
+	/**
+	 * create gallery loader
+	 */
+	Site.gallery_loader = new Caracal.Gallery.Loader();
+	Site.gallery_loader
+			.add_gallery(Site.portfolio_gallery)
+			.set_constructor(make_image)
+			.set_thumbnail_size(350 ,Caracal.Gallery.Constraint.HEIGHT)
+			.add_callback(image_loaded)
+
+	/**
+	 * create click event for loading gallery loader
+	 */
+	var gallery_list = $('section#portfolio ul li a');
+	gallery_list.on('click',function(event) {
+		event.preventDefault();
+		var item = $(this);
+		gallery_list.not(item).removeClass('active')
+		var gallery_id = item.data('gallery');
+		console.log(gallery_id);
+		Site.gallery_loader.load_by_group_id(gallery_id);
+		item.addClass('active');
+	});
 };
 
 
