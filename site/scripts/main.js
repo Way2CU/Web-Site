4633fb9e2bf4265bb6e4a031175e2c6a98bc4f44
talrez <tal@way2cu.com>
2016-09-15 18:19:45 +0300

    Fixed indentation

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -60,7 +60,7 @@ Site.handle_scroll = function(event) {
 			'top': '90px'
 		});
 
-	} else if (scroll_position > Site.end_position) {
+	} else if(scroll_position > Site.end_position) {
 		opacity = 1;
 		Site.logo_elements.css({
 			'position': 'absolute',
